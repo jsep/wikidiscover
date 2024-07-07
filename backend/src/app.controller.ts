d5b4740b59fbac2e37f0d3073a14782edd652cb7
Juan Sepulveda <juan@easyappshq.com>
2024-07-07 15:58:00 -0400

    Validate params

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('feed/:lang/featured/:year/:month/:day')
+  async getFeed(
+    @Param('lang') lang: string,
+    @Param('year') year: string,
+    @Param('month') month: string,
+    @Param('day') day: string,
+  ): Promise<{ error: string | null; data: string | null }> {
+    try {
+      const data = await this.appService.getFeed({ lang, year, month, day });
+      return { error: null, data };
+    } catch (error) {
+      return { error: error.message, data: null };
+    }
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { wikipediaLanguages } from './languages';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World! - new deploy';
+  supportedLanguages: string[];
+  constructor() {
+    this.supportedLanguages = wikipediaLanguages.map(
+      (language) => language.code,
+    );
+  }
+
+  getSupportedLanguages() {
+    return wikipediaLanguages;
+  }
+
+  async getFeed({
+    year,
+    month,
+    day,
+    lang,
+  }: {
+    lang: string;
+    year: string;
+    month: string;
+    day: string;
+  }) {
+    if (!this.isValidDate(year, month, day)) {
+      throw new Error('Invalid date');
+    }
+    if (!this.supportedLanguages.includes(lang)) {
+      throw new Error(
+        `Unsupported language. \n Supported languages: ${this.supportedLanguages.join(',')}`,
+      );
+    }
+    return `${year}-${month}-${day} Lang:${lang}`;
+  }
+
+  private isValidDate(year: string, month: string, day: string): boolean {
+    // const date = new Date(Date.UTC(year, month, day));
+    const date = new Date(`${year}-${month}-${day}`);
+    return (
+      date.getUTCFullYear() === parseInt(year) &&
+      date.getUTCMonth() + 1 === parseInt(month) &&
+      date.getUTCDate() === parseInt(day)
+    );
   }
 }
