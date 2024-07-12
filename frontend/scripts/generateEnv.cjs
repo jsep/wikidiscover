a40aaaa6e45088c2a371bb7b824f859a3dc4d47a
Juan Sepulveda <juan@easyappshq.com>
2024-07-12 01:27:50 -0400

    Fix env var for frontend

--- frontend/scripts/generateEnv.cjs
+++ frontend/scripts/generateEnv.cjs
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const envFilePath = path.join(__dirname, '../.env');
-const envVars = ['TRANSLATE_API_URL', 'REDIS_URL'];
+const envVars = ['VITE_API_URL'];
 
 const envContent = envVars
   .map((envVar) => {
