9690eac034f1ff679a95181ba07818fce26b2645
Juan Sepulveda <juan@easyappshq.com>
2024-07-12 01:04:04 -0400

    Fix bug with frontend .env file

--- frontend/scripts/generateEnv.cjs
+++ frontend/scripts/generateEnv.cjs
@@ -2,7 +2,13 @@ const fs = require('fs');
 const path = require('path');
 
 const envFilePath = path.join(__dirname, '../.env');
-const envContent = `VITE_API_URL=${process.env.VITE_API_URL || ''}\n`;
+const envVars = ['TRANSLATE_API_URL', 'REDIS_URL'];
+
+const envContent = envVars
+  .map((envVar) => {
+    return `${envVar}=${process.env[envVar] || ''}`;
+  })
+  .join('\n');
 
 fs.writeFileSync(envFilePath, envContent, 'utf8');
 console.log(`.env file generated at ${envFilePath}`);
