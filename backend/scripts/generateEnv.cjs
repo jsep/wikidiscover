6154c893d5bfc5346243c63771d74f9a6e9abe4c
Juan Sepulveda <juan@easyappshq.com>
2024-07-12 01:01:37 -0400

    Update fly.toml when generarte .env

--- backend/scripts/generateEnv.cjs
+++ backend/scripts/generateEnv.cjs
@@ -13,3 +13,11 @@ const envContent = envVars
 fs.writeFileSync(envFilePath, envContent, 'utf8');
 console.log(`.env file generated at ${envFilePath}`);
 console.log(envContent);
+
+const flyTomlPath = path.join(__dirname, '../fly.toml');
+const flyTomlContent = fs.readFileSync(flyTomlPath, 'utf8');
+
+const updatedFlyTomlContent = `${flyTomlContent}\n[env]\n${envVars.map((envVar) => `  ${envVar}=${process.env[envVar] || ''}`).join('\n')}`;
+fs.writeFileSync(flyTomlPath, updatedFlyTomlContent, 'utf8');
+
+console.log(`fly.toml file updated at ${flyTomlPath}`);
